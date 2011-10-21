27de697727b2a223ce6703a7ac02c4609d39f443
bwu <brian.george.wu@gmail.com>
2011-10-21 17:28:15 -0400

    second commit

---
+++ jobs/timetest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+import pymongo
+from pymongo import Connection
+import json
+
+
+# Open the environment file and get the name of the host and port
+with open('/home/dotcloud/environment.json') as f:
+  env = json.load(f)
+
+# Assign the host and port
+host = env['DOTCLOUD_DATA_MONGODB_URL']
+
+# Connect to the database and assign the correct name for the database and collection
+connection = Connection(host=host)
+db = connection.tests
+collection = db.times
+
+# Give todays date as an item in a dictionary.
+today = { 'date and time' : datetime.today() }
+collection.insert(today)
